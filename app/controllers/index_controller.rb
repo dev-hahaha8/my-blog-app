18b5e20ee6768643141abc495eda0b3691c39a53
knt572 <572.r.knt@gmail.com>
2024-03-25 23:00:35 +0900

    Update CSS

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,41 @@
 class IndexController < ApplicationController
   def top
+    @articles = [
+      {
+        href: "/blog/css",
+        img_src: "/see.png",
+        title: "CSSの疑似要素"
+      },
+      {
+        href: "/blog/github",
+        img_src: "/kao_star.png",
+        title: "GitHubリポジトリの基本的なコマンド"
+      },
+      {
+        href: "/blog/gem",
+        img_src: "/odango.png",
+        title: "RailsでGemを活用した開発をしたい"
+      }
+    ]
   end
 
   def blog
+    @articles = [
+      {
+        href: "/blog/css",
+        img_src: "/see.png",
+        title: "CSSの疑似要素"
+      },
+      {
+        href: "/blog/github",
+        img_src: "/kao_star.png",
+        title: "GitHubリポジトリの基本的なコマンド"
+      },
+      {
+        href: "/blog/gem",
+        img_src: "/odango.png",
+        title: "RailsでGemを活用した開発をしたい"
+      }
+    ]
   end
 end
